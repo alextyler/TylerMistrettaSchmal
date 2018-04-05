f48785aca9183b81ba0f6a98011458d3873014e3
alextyler <37344105+alextyler@users.noreply.github.com>
2018-04-05 12:13:30 -0500

    Add files via upload

---
+++ project_1/private/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+// database constants
+// you need to change these values to match your Database
+// these values need a ""
+define("DB_SERVER", "localhost");
+define("DB_USER", "thoma662_pro1");
+define("DB_PASS", "ZtwuKd1234");
+define("DB_NAME", "thoma662_pro1");
+
+?>
